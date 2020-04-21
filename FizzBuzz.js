2b2da07d96648f0fcddef874dc356ca7315cf315
ganyobi13 <40219862+ganyobi13@users.noreply.github.com>
2020-04-21 14:17:33 +0000

    Create FizzBuzz.js

---
+++ FizzBuzz.js
@@ -0,0 +1,17 @@
+// Eloquent JavaScript
+// Exercise 1B
+// FizzBuzz
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0) {
+    if (i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else {
+        console.log("Fizz");
+    }
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
